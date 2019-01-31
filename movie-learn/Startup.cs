b8d98232c235a77db16959e20db0fcdc1a803030
lovsokheang <lovsokheang@gmail.com>
2019-01-31 11:13:50 +0700

    Add project files.

---
+++ movie-learn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(movie_learn.Startup))]
+namespace movie_learn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
